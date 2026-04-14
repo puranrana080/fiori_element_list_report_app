e52886c57f227a9bbb38f17989b2c86cdb3d0b08
Puran Rana <puransinghrana080@gmail.com>
2026-04-14 17:00:56 +0000

    v5 create custom filter,validate

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,83 @@
+sap.ui.define([
+    "sap/ui/model/Filter",
+    "sap/ui/comp/smartfilterbar/SmartFilterBar",
+    "sap/m/MultiComboBox",
+    "sap/m/MessageBox"
+], function (Filter, SmartFilterBar, MultiComboBox, MessageBox) {
+    "use strict";
+    return {
+        getCustomAppStateDataExtension: function (oCustomData) {
+            //the content of the custom field will be stored in the app state, so that it can be restored later, for example after a back navigation.
+            //The developer has to ensure that the content of the field is stored in the object that is passed to this method.
+            if (oCustomData) {
+                var oCustomField1 = this.oView.byId("Status");
+                if (oCustomField1) {
+                    oCustomData.Status = oCustomField1.getSelectedKeys();
+                }
+            }
+        },
+        restoreCustomAppStateDataExtension: function (oCustomData) {
+            //in order to restore the content of the custom field in the filter bar, for example after a back navigation,
+            //an object with the content is handed over to this method. Now the developer has to ensure that the content of the custom filter is set to the control
+            if (oCustomData) {
+                if (oCustomData.Status) {
+                    var oComboBox = this.oView.byId("Status");
+                    oComboBox.setSelectedKeys(
+                        oCustomData.Status
+                    );
+                }
+            }
+        },
+        onBeforeRebindTableExtension: function (oEvent) {
+            var oBindingParams = oEvent.getParameter("bindingParams");
+            oBindingParams.parameters = oBindingParams.parameters || {};
+            var oSmartTable = oEvent.getSource();
+            var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+            if (oSmartFilterBar instanceof SmartFilterBar) {
+                var oCustomControl = oSmartFilterBar.getControlByKey("Status");
+
+                var desigFilter = oSmartFilterBar.getControlByKey("Desig");
+                if (desigFilter.getTokens().length === 0) {
+                    MessageBox.error("Designation filter is mandatory")
+                    oBindingParams.preventTableBind = true
+                }
+                else {
+                    if (oCustomControl instanceof MultiComboBox) {
+                        var aStatus = oCustomControl.getSelectedKeys();
+                        if (aStatus.length > 0) {
+                            var aStatusFilters = []
+                            for (var i = 0; i < aStatus.length; i++) {
+                                aStatusFilters.push(new Filter("Status", "EQ", aStatus[i]))
+                            }
+
+                            oBindingParams.filters.push(new Filter(aStatusFilters, false))
+                        }
+                        if (oBindingParams.filters.length === 0) {
+                            MessageBox.error("Please select atleast one filter and Click Go Button")
+                            oBindingParams.preventTableBind = true
+                        }
+
+                        // if(status!==""){
+                        //     oBindingParams.filters.push(new Filter("Status","EQ",status))
+                        // }
+                        // switch (vCategory) {
+                        //     // case "0" :
+                        //     // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "SAP"));
+                        //     // 	break;
+                        //     // case "1" :
+                        //     // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "OTHERS"));
+                        //     // 	break;
+                        //     default:
+                        //         break;
+                        // }
+                    }
+
+                }
+
+
+
+
+            }
+        }
+    };
+});
